228056a6db9c7e5c7794df2e79936014d6231181
Runtas, James <jamesruntas@cmail.carleton.ca>
2022-02-10 14:10:59 -0500

    removed excess

--- Assignment2/Client.java
+++ Assignment2/Client.java
@@ -10,9 +10,6 @@ public class Client {
    public Client()
    {
       try {
-         // Construct a datagram socket and bind it to any available 
-         // port on the local host machine. This socket will be used to
-         // send and receive UDP Datagram packets.
          sendReceiveSocket = new DatagramSocket();
       } catch (SocketException se) {   // Can't create the socket.
          se.printStackTrace();
@@ -22,32 +19,10 @@ public class Client {
 
    public void sendAndReceive()
    {
-      // Prepare a DatagramPacket and send it via sendReceiveSocket
-      // to port 5000 on the destination host.
- 
       String s = "Anyone there?";
       System.out.println("Client: sending a packet containing:\n" + s);
 
-      // Java stores characters as 16-bit Unicode values, but 
-      // DatagramPackets store their messages as byte arrays.
-      // Convert the String into bytes according to the platform's 
-      // default character encoding, storing the result into a new 
-      // byte array.
-
       byte msg[] = s.getBytes();
-
-      // Construct a datagram packet that is to be sent to a specified port 
-      // on a specified host.
-      // The arguments are:
-      //  msg - the message contained in the packet (the byte array)
-      //  msg.length - the length of the byte array
-      //  InetAddress.getLocalHost() - the Internet address of the 
-      //     destination host.
-      //     In this example, we want the destination to be the same as
-      //     the source (i.e., we want to run the client and server on the
-      //     same computer). InetAddress.getLocalHost() returns the Internet
-      //     address of the local host.
-      //  5000 - the destination port number on the destination host.
       try {
          sendPacket = new DatagramPacket(msg, msg.length,
                                          InetAddress.getLocalHost(), 5000);
@@ -62,9 +37,8 @@ public class Client {
       int len = sendPacket.getLength();
       System.out.println("Length: " + len);
       System.out.print("Containing: ");
-      System.out.println(new String(sendPacket.getData(),0,len)); // or could print "s"
+      System.out.println(new String(sendPacket.getData(),0,len)); 
 
-      // Send the datagram packet to the server via the send/receive socket. 
 
       try {
          sendReceiveSocket.send(sendPacket);
@@ -74,9 +48,6 @@ public class Client {
       }
 
       System.out.println("Client: Packet sent.\n");
-
-      // Construct a DatagramPacket for receiving packets up 
-      // to 100 bytes long (the length of the byte array).
 
       byte data[] = new byte[100];
       receivePacket = new DatagramPacket(data, data.length);
