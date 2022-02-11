3497f1e122223d2c0ca07df81f90a68afe6b093e
James <jamesruntas@cmail.carleton.ca>
2022-02-10 19:53:29 -0500

    package

---
+++ Assignment2/Client.java
@@ -0,0 +1,86 @@
+package Assignment2;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+   DatagramPacket sendPacket, receivePacket;
+   DatagramSocket sendReceiveSocket;
+
+   public Client()
+   {
+      try {
+         sendReceiveSocket = new DatagramSocket();
+      } catch (SocketException se) {   // Can't create the socket.
+         se.printStackTrace();
+         System.exit(1);
+      }
+   }
+
+   public void sendAndReceive()
+   {
+      String s = "Anyone there?";
+      System.out.println("Client: sending a packet containing:\n" + s);
+
+      byte msg[] = s.getBytes();
+      try {
+         sendPacket = new DatagramPacket(msg, msg.length,
+                                         InetAddress.getLocalHost(), 5000);
+      } catch (UnknownHostException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+
+      System.out.println("Client: Sending packet:");
+      System.out.println("To host: " + sendPacket.getAddress());
+      System.out.println("Destination host port: " + sendPacket.getPort());
+      int len = sendPacket.getLength();
+      System.out.println("Length: " + len);
+      System.out.print("Containing: ");
+      System.out.println(new String(sendPacket.getData(),0,len)); 
+
+
+      try {
+         sendReceiveSocket.send(sendPacket);
+      } catch (IOException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+
+      System.out.println("Client: Packet sent.\n");
+
+      byte data[] = new byte[100];
+      receivePacket = new DatagramPacket(data, data.length);
+
+      try {
+         // Block until a datagram is received via sendReceiveSocket.  
+         sendReceiveSocket.receive(receivePacket);
+      } catch(IOException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+
+      // Process the received datagram.
+      System.out.println("Client: Packet received:");
+      System.out.println("From host: " + receivePacket.getAddress());
+      System.out.println("Host port: " + receivePacket.getPort());
+      len = receivePacket.getLength();
+      System.out.println("Length: " + len);
+      System.out.print("Containing: ");
+
+      // Form a String from the byte array.
+      String received = new String(data,0,len);   
+      System.out.println(received);
+
+      // We're finished, so close the socket.
+      sendReceiveSocket.close();
+   }
+
+   public static void main(String args[])
+   {
+      System.out.println("Client.java");
+      Client c = new Client();
+      c.sendAndReceive();
+   }
+}
+
